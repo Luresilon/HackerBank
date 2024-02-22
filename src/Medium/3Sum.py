e2b12f29deb13ac3901f1a523f0600eb7ae15782
Luresilon <ababiisorin@hotmail.com>
2024-02-22 22:44:10 +0000

    Create 3Sum

---
+++ src/Medium/3Sum.py
@@ -0,0 +1,16 @@
+
+def threeSum(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    arr = []
+
+    for i, a in enumerate(nums):
+        print(i, a)
+    return arr
+        
+nums = [-1,0,1,2,-1,-4]
+
+ans = threeSum(nums)
+# 
