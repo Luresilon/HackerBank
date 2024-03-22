f0e019eab763f452d8032e9ed63fe168d0814842
Luresilon <ababiisorin@hotmail.com>
2024-03-22 15:09:24 +0000

    Added CharFrequencySort

---
+++ src/Medium/CharFrequencySort.java
@@ -0,0 +1,66 @@
+package Medium;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CharFrequencySort {
+
+//    public static String frequencySort(String s) {
+//        Map<Character, Integer> map = s.chars()
+//                .mapToObj(c -> (char) c)
+//                .collect(
+//                        Collectors.groupingBy(
+//                                Function.identity(),
+//                                Collectors.summingInt(c -> 1)
+//                        )
+//                );
+//
+//        LinkedHashMap<Character, Integer> sortedMap = map.entrySet().stream()
+//                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+//                .collect(Collectors.toMap(
+//                        Map.Entry::getKey,
+//                        Map.Entry::getValue,
+//                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+//
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//        for(var entry: sortedMap.entrySet()) {
+//            System.out.println(entry.getKey());
+//            stringBuilder.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+//        }
+//        return stringBuilder.toString();
+//    }
+
+    public static String frequencySort(String s) {
+        Map<Character, Integer> map = s.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(c -> 1)));
+
+        LinkedHashMap<Character, Integer> sortedMap = map.entrySet().stream()
+                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(var entry: sortedMap.entrySet()) {
+            System.out.println(entry.getValue());
+            stringBuilder.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+        }
+
+
+        return stringBuilder.toString();
+    }
+
+
+    public static void main(String[] args) {
+        String s = "tree";
+
+        System.out.println(frequencySort(s));
+    }
+}
--- src/Medium/TopKFrequentElements.java
+++ src/Medium/TopKFrequentElements.java
@@ -1,19 +1,68 @@
 package Medium;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class TopKFrequentElements {
 
+//    public static int[] topKFrequent(int[] nums, int k) {
+//        return Arrays.stream(nums)
+//                .boxed()
+//                .collect(Collectors.groupingBy(it -> it, Collectors.counting()))
+//                .entrySet()
+//                .stream()
+//                .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
+//                .limit(k)
+//                .mapToInt(Map.Entry::getKey).toArray();
+//    }
+
+//    public static int[] topKFrequent(int[] nums, int k) {
+//        Map<Integer, Long> map = Arrays.stream(nums).boxed().collect(
+//                Collectors.groupingBy(
+//                        Function.identity(),
+//                        Collectors.counting()
+//                )
+//        );
+//
+//        return map.entrySet().stream()
+//                .sorted(Map.Entry.<Integer, Long>comparingByValue().reversed())
+//                .limit(k)
+////                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+//                .mapToInt(Map.Entry::getKey).toArray();
+//
+////        return new int[] {};
+//    }
+
+
+
+
     public static int[] topKFrequent(int[] nums, int k) {
-        return Arrays.stream(nums).boxed().collect(Collectors.groupingBy(it -> it, Collectors.counting())).entrySet().stream().sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue())).limit(k).mapToInt(Map.Entry::getKey).toArray();
+        Map<Integer, Long> map = Arrays.stream(nums)
+                .boxed()
+                .collect(
+                        Collectors.groupingBy(
+                                Function.identity(),
+                                Collectors.counting()
+                        )
+                );
+
+        return map.entrySet().stream()
+                .sorted(Map.Entry.<Integer, Long>comparingByValue().reversed())
+                .limit(k)
+                .mapToInt(Map.Entry::getKey).toArray();
     }
 
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
-        int[] nums = {1, 1, 1, 2, 2, 3};
+        int[] nums = {1, 1, 1, 2, 2,2,2, 3};
         int k = 2;
 
         System.out.println(Arrays.toString(topKFrequent(nums, k)));
