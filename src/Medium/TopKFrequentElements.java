4fef06dd23f13abce411bb0b7b64b0372f4d4a30
Luresilon <ababiisorin@hotmail.com>
2024-04-04 17:29:52 +0100

    Update TopKFrequentElements.java

--- src/Medium/TopKFrequentElements.java
+++ src/Medium/TopKFrequentElements.java
@@ -6,37 +6,6 @@ import java.util.stream.Collectors;
 
 public class TopKFrequentElements {
 
-//    public static int[] topKFrequent(int[] nums, int k) {
-//        return Arrays.stream(nums)
-//                .boxed()
-//                .collect(Collectors.groupingBy(it -> it, Collectors.counting()))
-//                .entrySet()
-//                .stream()
-//                .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
-//                .limit(k)
-//                .mapToInt(Map.Entry::getKey).toArray();
-//    }
-
-//    public static int[] topKFrequent(int[] nums, int k) {
-//        Map<Integer, Long> map = Arrays.stream(nums).boxed().collect(
-//                Collectors.groupingBy(
-//                        Function.identity(),
-//                        Collectors.counting()
-//                )
-//        );
-//
-//        return map.entrySet().stream()
-//                .sorted(Map.Entry.<Integer, Long>comparingByValue().reversed())
-//                .limit(k)
-////                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
-//                .mapToInt(Map.Entry::getKey).toArray();
-//
-////        return new int[] {};
-//    }
-
-
-
-
     public static int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Long> map = Arrays.stream(nums)
                 .boxed()
