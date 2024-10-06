c35653c1a2327b9aa4cea7db09a1d9174d83f5a9
Luresilon <ababiisorin@hotmail.com>
2024-10-06 13:01:21 +0100

    Update MaximizeScoreOfNumbersInRanges.java

--- src/BinarySearch/Medium/MaximizeScoreOfNumbersInRanges.java
+++ src/BinarySearch/Medium/MaximizeScoreOfNumbersInRanges.java
@@ -5,43 +5,43 @@ import java.util.Arrays;
 
 public class MaximizeScoreOfNumbersInRanges {
 
-    private static int maxPossibleScore(int[] start, int d) {
-        int len = start.length;
-        Arrays.sort(start);
+//    private static int maxPossibleScore(int[] start, int d) {
+//        int len = start.length;
+//        Arrays.sort(start);
+//
+//        long l = 0, res = 0;
+//        long r = (long) start[len - 1] + d - start[0];
+//
+//        while(l <= r) {
+//            long mid = l + (r - l) / 2;
+//            if(achievable(start, d, mid)) {
+//                res = mid;
+//                l = mid + 1;
+//            } else {
+//                r = mid - 1;
+//            }
+//        }
+//        return (int) res;
+//    }
 
-        long l = 0, res = 0;
-        long r = (long) start[len - 1] + d - start[0];
-
-        while(l <= r) {
-            long mid = l + (r - l) / 2;
-            if(achievable(start, d, mid)) {
-                res = mid;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-        return (int) res;
-    }
-
-    private static boolean achievable(int[] start, int d, long mid) {
-        long prev = start[0];
-
-        for(int i = 1; i < start.length; i++) {
-            long next = Math.max(prev + mid, start[i]);
-
-            if(next > (long) start[i] + d) return false;
-
-            prev = next;
-            int g = Integer.toBinaryString(10);
-        }
-        return true;
-    }
+//    private static boolean achievable(int[] start, int d, long mid) {
+//        long prev = start[0];
+//
+//        for(int i = 1; i < start.length; i++) {
+//            long next = Math.max(prev + mid, start[i]);
+//
+//            if(next > (long) start[i] + d) return false;
+//
+//            prev = next;
+//            int g = Integer.toBinaryString(10);
+//        }
+//        return true;
+//    }
 
     public static void main(String[] args) {
         int[] start = {2, 6, 13, 13};
         int d  = 5;
 
-        System.out.println(maxPossibleScore(start, d));
+//        System.out.println(maxPossibleScore(start, d));
     }
 }
