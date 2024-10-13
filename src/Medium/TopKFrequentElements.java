ab49e7b672d51550d11a4fb304a05889b7527a44
Luresilon <ababiisorin@hotmail.com>
2024-10-14 00:24:39 +0100

    Update TopKFrequentElements.java

--- src/Medium/TopKFrequentElements.java
+++ src/Medium/TopKFrequentElements.java
@@ -22,10 +22,21 @@ public class TopKFrequentElements {
                 .mapToInt(Map.Entry::getKey).toArray();
     }
 
+    public static int[] topKFrequentTwo(int[] nums, int k) {
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for(int num: nums) frequencyMap.merge(num, 1, Integer::sum);
+
+        return frequencyMap.entrySet().stream()
+                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+                .limit(k)
+                .mapToInt(Map.Entry::getKey)
+                .toArray();
+    }
     public static void main(String[] args) {
         int[] nums = {1, 1, 1, 2, 2,2,2, 3,3 ,3, 3};
         int k = 2;
 
-        System.out.println(Arrays.toString(topKFrequent(nums, k)));
+        System.out.println(Arrays.toString(topKFrequentTwo(nums, k)));
     }
 }
