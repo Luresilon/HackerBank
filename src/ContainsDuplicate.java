1fd1f452750552bc3a6aa377e60da342e9004e04
Luresilon <ababiisorin@hotmail.com>
2024-10-14 00:25:19 +0100

    Update ContainsDuplicate.java

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -4,11 +4,17 @@ public class ContainsDuplicate {
 
     public static boolean containsDuplicate(int[] nums) {
         Set<Integer> numSet = new HashSet<>();
-        return Arrays.stream(nums).anyMatch(num -> !numSet.add(num));
+
+        for(int i = 0; i < nums.length; i++) {
+            int tmp = nums[i];
+            if(!numSet.contains(tmp)) numSet.add(tmp);
+            else return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,1};
+        int[] arr = {1,1,1,3,3,4,3,2,4,2};
         System.out.println(containsDuplicate(arr));
     }
 }
