1a318eefe2d296a0475902cb1d7f8b73138ee49f
Luresilon <ababiisorin@hotmail.com>
2024-10-14 00:24:57 +0100

    Update GroupAnagrams.java

--- src/Medium/GroupAnagrams.java
+++ src/Medium/GroupAnagrams.java
@@ -5,41 +5,66 @@ import java.util.*;
 public class GroupAnagrams {
     private static List<List<String>> res;
 
+//    public static List<List<String>> groupAnagrams(String[] strs) {
+//        return new AbstractList<List<String>>() {
+//            public List<String> get(int index) {
+//                if (res == null) init();
+//                return res.get(index);
+//            }
+//
+//            public int size() {
+//                if (res == null) init();
+//                return res.size();
+//            }
+//
+//            private void init() {
+//                Map<String, List<String>> map = new HashMap<>();
+//                for (String s : strs) {
+//                    int[] count = new int[26];
+//                    for (char c : s.toCharArray()) {
+//
+//                        count[c - 'a']++;
+//                    }
+//                    StringBuilder sb = new StringBuilder();
+//                    for (int i = 0; i < 26; i++) {
+//                        if (count[i] != 0) {
+//                            sb.append('a' + i);
+//                            sb.append(count[i]);
+//                        }
+//                    }
+//                    String key = sb.toString();
+//                    map.computeIfAbsent(key, k -> new ArrayList<String>()).add(s);
+//                    res = new ArrayList<>(map.values());
+//                }
+//            }
+//        };
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static List<List<String>> groupAnagrams(String[] strs) {
-        return new AbstractList<List<String>>() {
-            public List<String> get(int index) {
-                if (res == null) init();
-                return res.get(index);
-            }
+        if(strs.length == 1) return new ArrayList<>(1){{add(new ArrayList<>(1){{add(strs[0]);}});}};
 
-            public int size() {
-                if (res == null) init();
-                return res.size();
-            }
+        HashMap<String, List<String>> sameChars = new HashMap<>();
 
-            private void init() {
-                Map<String, List<String>> map = new HashMap<>();
-                for (String s : strs) {
-                    int[] count = new int[26];
-                    for (char c : s.toCharArray()) {
+        for(String str: strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
 
-                        count[c - 'a']++;
-                    }
-                    StringBuilder sb = new StringBuilder();
-                    for (int i = 0; i < 26; i++) {
-                        if (count[i] != 0) {
-                            sb.append('a' + i);
-                            sb.append(count[i]);
-                        }
-                    }
-                    String key = sb.toString();
-                    map.computeIfAbsent(key, k -> new ArrayList<String>()).add(s);
-                    res = new ArrayList<>(map.values());
-                }
-            }
-        };
+            sameChars.computeIfAbsent(new String(chars), k -> new ArrayList<>(sameChars.size())).add(str);
+        }
+        return new ArrayList<>(sameChars.values());
     }
-
     public static void main(String[] args) {
         String[] str = {"eat","tea","tan","ate","nat","bat"};
         List<List<String>> strList = groupAnagrams(str);
