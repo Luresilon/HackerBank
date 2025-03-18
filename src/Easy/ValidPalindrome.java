f0d4c577e948aa4d9cd24758ea55d5f5941380a5
Luresilon <ababiisorin@hotmail.com>
2025-03-18 20:41:05 +0000

    Update ValidPalindrome.java

--- src/Easy/ValidPalindrome.java
+++ src/Easy/ValidPalindrome.java
@@ -1,14 +1,23 @@
 package Easy;
 
+import java.util.Arrays;
+
 public class ValidPalindrome {
     public static Boolean isPalindrome(String pal) {
         pal = pal.replaceAll("[^/a-zA-Z0-9]", "").toLowerCase();
-        System.out.println(pal);
         return pal.equals(new StringBuilder(pal).reverse().toString().toLowerCase());
     }
+    public static Boolean isPalindromeTwo(String pal) {
+        String strA = pal.replaceAll("[^/a-zA-Z0-9]", "");
+
+        return false;
+    }
+
+
+
 
     public static void main(String[] args) {
         String pal = "ab_a..";
-        System.out.println(isPalindrome(pal));
+        System.out.println(isPalindromeTwo(pal));
     }
 }
