907c7ae7062bef2109b725c595bae0e543b3f12f
Luresilon <ababiisorin@hotmail.com>
2025-03-19 21:25:04 +0000

    Create CountVowelStringsInRanges.java

---
+++ src/Medium/CountVowelStringsInRanges.java
@@ -0,0 +1,31 @@
+//package Medium;
+//
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+////Leetcode problem: 2559
+//public class CountVowelStringsInRanges {
+//
+//
+//    public static int[] vowelStrings(String[] words, int[][] queries) {
+//        HashMap<int[], Integer> counter = new HashMap<>();
+//        Set<Character> vowels = new
+//
+//
+//        for(String word: words) {
+//            if(!vowels.contains(word.charAt(0)) && vowels.contains(word.charAt(word.length() - 1))) continue;
+//
+//            counter.putIfAbsent(new int[])
+//        }
+//
+//
+//        return new int[]{};
+//    }
+//    public static void main(String[] args) {
+//        String[] words = {"aba","bcb","ece","aa","e"};
+//        int[][] queries = {{0,2},{1,4},{1,1}};
+//        System.out.println(Arrays.toString(vowelStrings(words, queries)));
+//    }
+//}
